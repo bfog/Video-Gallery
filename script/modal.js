e41c514b5dc7014f9195d8b8851384f90d54ba2d
bfog <31835696+bfog@users.noreply.github.com>
2018-06-01 20:32:50 +1000

    Update modal.js

--- script/modal.js
+++ script/modal.js
@@ -1,17 +1,3 @@
-$(document).ready(function(){
-    /* Get iframe src attribute value i.e. YouTube video url
-    and store it in a variable */
-    var url = $("#firstVid").attr('src');
-    
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
-    $("#myModal").on('hide.bs.modal', function(){
-        $("#firstVid").attr('src', '');
-    });
-    
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed again */
-    $("#myModal").on('show.bs.modal', function(){
-        $("#firstVid").attr('src', url);
-    });
+$('body').on('hidden.bs.modal', '.modal', function () {
+$('video').trigger('pause');
 });
